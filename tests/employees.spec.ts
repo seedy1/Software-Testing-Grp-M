8ed8033ab8b5dda769c39c9182343af2c0fe4320
Samuel VIDEAU <samuel.videau@efrei.net>
2022-02-08 16:20:14 +0100

    chore: employees tests

---
+++ tests/employees.spec.ts
@@ -0,0 +1,130 @@
+import {expect, test} from "@playwright/test";
+import {generateRandString} from "./utils/rand-string";
+
+async function createEmployee(page: any, str: string) {
+  await page.goto('https://m.hr.dmerej.info/add_employee');
+  await page.fill(`input[name='name']`, str);
+  await page.fill(`input[name='email']`, str + '@test.test');
+  await page.fill(`input[name='address_line1']`, str);
+  await page.fill(`input[name='address_line2']`, str);
+  await page.fill(`input[name='city']`, str);
+  await page.fill(`input[name='zip_code']`, '92130');
+  await page.fill(`input[name='hiring_date']`, '01/10/2012');
+  await page.fill(`input[name='job_title']`, str);
+  await page.click('.btn-primary');
+}
+
+test.describe('Managing employees', async () => {
+  let employeeName = '';
+
+  test.beforeEach(async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/reset_db');
+    await page.click('.btn-danger');
+
+    employeeName = generateRandString('add');
+    await createEmployee(page, employeeName);
+  });
+
+  test('can create new employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+    expect(await page.content()).toContain(employeeName);
+  });
+
+  test('can edit basic info of an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update basic info '`);
+
+    const newRandString: string = generateRandString('edit');
+
+    await page.fill(`input[name='name']`, newRandString);
+    await page.fill(`input[name='email']`,  newRandString + '@test.test');
+
+    await page.click('.btn-primary');
+
+    await expect(page).toHaveURL('https://m.hr.dmerej.info/employees');
+    expect(await page.content()).toContain(newRandString);
+  });
+
+  test('can edit address line 1 of an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    const newRandString: string = generateRandString('edit');
+
+    await page.fill(`input[name='address_line1']`, newRandString);
+
+    await page.click('.btn-primary');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    expect(await page.inputValue(`input[name='address_line1']`)).toEqual(newRandString);
+  });
+
+  test('can edit address line 2 of an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    const newRandString: string = generateRandString('edit');
+
+    await page.fill(`input[name='address_line2']`, newRandString);
+
+    await page.click('.btn-primary');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    expect(await page.inputValue(`input[name='address_line2']`)).toEqual(newRandString);
+  });
+
+  test('can edit city of an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    const newRandString: string = generateRandString('edit');
+
+    await page.fill(`input[name='city']`, newRandString);
+
+    await page.click('.btn-primary');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    expect(await page.inputValue(`input[name='city']`)).toEqual(newRandString);
+  });
+
+  test('can edit zip code of an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    const editRand: string = (Math.floor(Math.random() * 999)).toString();
+
+    await page.fill(`input[name='zip_code']`, editRand);
+
+    await page.click('.btn-primary');
+
+    await page.click('.btn-primary  >> nth=-1');
+    await page.click(`text=' Update address '`);
+
+    expect(await page.inputValue(`input[name='zip_code']`)).toEqual(editRand);
+  });
+
+  test('can delete an employee', async ({page}) => {
+    await page.goto('https://m.hr.dmerej.info/employees');
+
+    await page.click(`text=' Delete ' >> nth=-1`);
+    await page.click(`text='Proceed'`);
+
+    expect(await page.content()).not.toContain(employeeName);
+  });
+});
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,7 @@
 import { test, expect } from '@playwright/test';
 import {generateRandString} from "./utils/rand-string";
 
-async function createEmployee(page: any, str: string) {
-  await page.fill(`input[name='name']`, str);
-  await page.fill(`input[name='email']`, str + '@test.test');
-  await page.fill(`input[name='address_line1']`, str);
-  await page.fill(`input[name='address_line2']`, str);
-  await page.fill(`input[name='city']`, str);
-  await page.fill(`input[name='zip_code']`, '92130');
-  await page.fill(`input[name='hiring_date']`, '01/10/2012');
-  await page.fill(`input[name='job_title']`, str);
-  await page.click('.btn-primary');
-}
-
 test.describe('Team M', () => {
-
-  // test.beforeEach(async ({page}) => {
-  //   await page.goto('https://m.hr.dmerej.info/reset_db');
-  //   await page.click('.btn-danger');
-  // });
 
   test('Have the right title', async ({ page }) => {
     await page.goto('https://m.hr.dmerej.info/');
@@ -33,37 +16,6 @@ test.describe('Team M', () => {
     await expect(page).toHaveURL('https://m.hr.dmerej.info/add_employee');
   });
 
-  test.describe('Managing employees', async () => {
-
-    test('can create new employee', async ({page}) => {
-      await page.goto('https://m.hr.dmerej.info/add_employee');
-
-      const randString: string = generateRandString('add');
-
-      await createEmployee(page, randString);
-
-      await expect(page).toHaveURL('https://m.hr.dmerej.info/employees');
-      expect(await page.content()).toContain(randString);
-    });
-
-    test('can edit basic info of an employee', async ({page}) => {
-      await page.goto('https://m.hr.dmerej.info/employees');
-      await page.click('.btn-primary');
-      await page.click(`text=' Update basic info '`);
-
-      const newRandString: string = generateRandString('edit');
-
-      await page.fill(`input[name='name']`, newRandString);
-      await page.fill(`input[name='email']`,  newRandString + '@test.test');
-
-      await page.click('.btn-primary');
-
-      await expect(page).toHaveURL('https://m.hr.dmerej.info/employees');
-      expect(await page.content()).toContain(newRandString);
-    });
-  });
-
-  
   test.describe('Team management', () => {
 
     const RandString: string = generateRandString('Team');
@@ -75,14 +27,14 @@ test.describe('Team M', () => {
       await expect(page).toHaveURL('https://m.hr.dmerej.info/teams');
       expect(await page.content()).toContain(RandString);
     });
-    
-    
+
+
     test('Delete a team', async ({ page }) => {
       await page.goto('https://m.hr.dmerej.info/teams');
       await page.click('.btn-danger');
       await page.locator('form button[type="submit"]');
       expect (await page.content()).not.toContain(RandString);
-    
+
     });
   })
 
